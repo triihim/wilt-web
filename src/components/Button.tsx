d7db1b2dd5475161e01cf3c233f03d44a49dfadf
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2024-01-16 18:53:47 +0200

    minor button-component styling refactor

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,10 +5,11 @@ type ButtonVariant = 'primary' | 'secondary';
 type ButtonStyle = 'common' | ButtonVariant;
 
 const buttonStyles: Record<ButtonStyle, string> = {
-  common: 'py-2 px-4 rounded-md font-bold disabled:opacity-50 border-gray-950 border-4 text-gray-950',
+  common:
+    'py-2 px-4 rounded-md font-bold disabled:opacity-50 border-gray-950 border-4 text-gray-950 whitespace-nowrap min-w-fit',
   primary: 'bg-emerald-300 disabled:bg-emerald-200 hover:bg-emerald-500 hover:text-white',
   secondary: 'hover:bg-emerald-300',
-} as const;
+};
 
 type ButtonProps = ComponentProps<'button'> & {
   variant: ButtonVariant;
