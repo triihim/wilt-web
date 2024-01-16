5b857abb734500b57286a89a2e99eb53d42a3551
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2024-01-16 18:55:43 +0200

    refactored Input-component to have a prop for inline label

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -2,13 +2,30 @@ import { ComponentProps } from 'react';
 
 type InputProps = ComponentProps<'input'> & {
   label?: string;
+  inlineLabel?: boolean;
 };
 
 export default function Input(props: InputProps) {
+  const { inlineLabel, label, className, ...rest } = props;
+
+  if (label && inlineLabel) {
+    return (
+      <div className="flex w-full">
+        <label className="p-2 order-first border-slate-300 border-2 bg-slate-200 rounded-l-md whitespace-nowrap">
+          {label}
+        </label>
+        <input
+          {...rest}
+          className={`p-2 border-slate-300 border-l-0 border-2 rounded-r-md w-full ${className}`}
+        ></input>
+      </div>
+    );
+  }
+
   return (
     <div>
-      {props.label && <label>{props.label}</label>}
-      <input className="p-2 border-slate-200 border-2 rounded-md w-full" {...props}></input>
+      {label && <label>{label}</label>}
+      <input {...rest} className={`p-2 border-slate-200 border-2 rounded-md w-full ${className}`}></input>
     </div>
   );
 }
