cbd4e7428d18fdc3e552627fc11e5962d0db462c
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2024-01-16 19:03:18 +0200

    changed app's width breakpoint from md to lg

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,7 +3,7 @@ import { ModalContextProvider } from '../components/modal/ModalContext';
 
 export function RootLayout() {
   return (
-    <main className="w-11/12 md:w-3/4 m-auto font-nunito">
+    <main className="w-11/12 lg:w-3/4 m-auto font-nunito transition-[width] duration-500">
       <ModalContextProvider>
         <Outlet />
       </ModalContextProvider>
