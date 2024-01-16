05a233d47379573fa718ba3ee338ec6983fcb817
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2024-01-16 19:02:23 +0200

    added ConfirmModal-component

---
+++ src/components/modal/ConfirmModal.tsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { ModalContext } from './ModalContext';
+import SubmitGroup from '../SubmitGroup';
+
+type ConfirmModalProps = {
+  prompt: string;
+  onConfirm(): void;
+  onCancel?(): void;
+};
+
+export default function ConfirmModal(props: ConfirmModalProps) {
+  const { setModalContent } = useContext(ModalContext);
+  return (
+    <div className="flex flex-col gap-5">
+      <p>{props.prompt}</p>
+      <SubmitGroup
+        onCancel={() => (props.onCancel ? props.onCancel() : setModalContent(null))}
+        onSubmit={props.onConfirm}
+        submitText={'Confirm'}
+      />
+    </div>
+  );
+}
