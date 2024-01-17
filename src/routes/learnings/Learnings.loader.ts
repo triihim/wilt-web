7edeae415bb3e92cd51958c7dc4e2b886bb73c2f
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2024-01-17 18:16:15 +0200

    refactored learnings route to use deferred loader data

--- src/routes/learnings/Learnings.loader.ts
+++ src/routes/learnings/Learnings.loader.ts
@@ -1,34 +1,23 @@
-import { LoaderFunctionArgs } from 'react-router-dom';
+import { LoaderFunctionArgs, defer } from 'react-router-dom';
 import { getLearningsPage } from '../../api/real';
 import { ILearning } from '../../types';
-import raiseError from '../../util/raiseError';
+
+export type LearningListResponse = {
+  learnings: Array<Pick<ILearning, 'id' | 'title' | 'createdAt'>>;
+  totalCount: number;
+};
 
 export type LoaderResponse = {
-  learnings: Array<Pick<ILearning, 'id' | 'title' | 'createdAt'>>;
-  totalLearningCount: number;
-  page: number;
+  learningsPromise: Promise<LearningListResponse>;
 };
 
 export const PAGE_SIZE = 5;
 
-export default async function loader(args: LoaderFunctionArgs): Promise<LoaderResponse> {
+export default async function loader(args: LoaderFunctionArgs) {
   const searchParams = new URL(args.request.url).searchParams;
 
   const page = +(searchParams.get('page') ?? 0);
   const titleFilter = searchParams.get('title') ?? '';
 
-  const learningsPage = await getLearningsPage(page, PAGE_SIZE, titleFilter);
-
-  // TODO: Cleanup / extract validation to its own function
-  if (
-    learningsPage &&
-    typeof learningsPage === 'object' &&
-    'learnings' in learningsPage &&
-    Array.isArray(learningsPage.learnings) &&
-    'totalCount' in learningsPage
-  ) {
-    return { learnings: learningsPage.learnings, totalLearningCount: learningsPage.totalCount as number, page };
-  } else {
-    raiseError('invalid-loader-response', 'Received invalid learnings');
-  }
+  return defer({ learningsPromise: getLearningsPage(page, PAGE_SIZE, titleFilter) });
 }
--- src/routes/learnings/Learnings.tsx
+++ src/routes/learnings/Learnings.tsx
@@ -1,35 +1,63 @@
-import { Link, useSearchParams } from 'react-router-dom';
+import { Await, Link, useSearchParams } from 'react-router-dom';
 import { ValidatorFunction } from '../../util/validators';
 import useAssertedLoaderData from '../../hooks/useAssertedLoaderData';
-import { LoaderResponse, PAGE_SIZE } from './Learnings.loader';
+import { LearningListResponse, LoaderResponse, PAGE_SIZE } from './Learnings.loader';
 import LearningListControls from './LearningListControls';
 import Button from '../../components/Button';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import useDebounce from '../../hooks/useDebounce';
 import { withClassAddedToMatchingSections } from '../../util/components';
+import { CenteredLoadingIndicator } from '../../components/LoadingIndicator';
+import ErrorView from '../ErrorView';
+import { ILearning } from '../../types';
+import raiseError from '../../util/raiseError';
 
 const FILTER_DEBOUNCE_TIME = 500;
 
-// Note: does not currently validate the structure of an individual learning.
-const loaderDataValidator: ValidatorFunction = (data) => !!data && typeof data === 'object' && 'learnings' in data;
+const loaderDataValidator: ValidatorFunction = (data) =>
+  !!data && typeof data === 'object' && 'learningsPromise' in data;
 
-export default function Learnings() {
+const isLearningListReponse = (obj: unknown): obj is LearningListResponse =>
+  !!obj && typeof obj === 'object' && 'learnings' in obj && 'totalCount' in obj;
+
+export default function LearningsPage() {
   const data = useAssertedLoaderData<LoaderResponse>(loaderDataValidator);
+  return (
+    <Suspense fallback={<CenteredLoadingIndicator />}>
+      <Await resolve={data.learningsPromise} errorElement={<ErrorView />}>
+        {(response) =>
+          isLearningListReponse(response) ? (
+            <LearningList learnings={response.learnings} totalLearningCount={response.totalCount} />
+          ) : (
+            raiseError('invalid-loader-response', 'Received invalid object to render as a learning learning list')
+          )
+        }
+      </Await>
+    </Suspense>
+  );
+}
+
+type LearningListProps = {
+  learnings: Array<Pick<ILearning, 'id' | 'title' | 'createdAt'>>;
+  totalLearningCount: number;
+};
+
+function LearningList(props: LearningListProps) {
   const [searchParams, setSearchParams] = useSearchParams();
   const [titleFilter, setTitleFilter] = useState(searchParams.get('title') || '');
   const debouncedTitleFilter = useDebounce(titleFilter, FILTER_DEBOUNCE_TIME);
 
-  const currentPage = data.page;
-  const totalPages = Math.ceil(data.totalLearningCount / PAGE_SIZE);
+  const currentPage = +(searchParams.get('page') ?? 0);
+  const totalPages = Math.ceil(props.totalLearningCount / PAGE_SIZE);
   const paginationText = 'Page ' + (totalPages > 0 ? `${currentPage + 1} / ${totalPages}` : '0 of 0'); // "Page X of Y" or "Page 0 of 0". Counts for 0 based pagination.
-  const hasNextPage = (currentPage + 1) * PAGE_SIZE < data.totalLearningCount; // +1 to adjust for pagination starting from 0 in code.
+  const hasNextPage = (currentPage + 1) * PAGE_SIZE < props.totalLearningCount; // +1 to adjust for pagination starting from 0 in code.
   const hasPreviousPage = currentPage > 0;
 
   const nextPage = () => {
     setSearchParams(
       (params) => {
         const currentPage = +(params.get('page') ?? 0);
-        const nextPage = Math.min(currentPage + 1, Math.floor(data.totalLearningCount / PAGE_SIZE));
+        const nextPage = Math.min(currentPage + 1, Math.floor(props.totalLearningCount / PAGE_SIZE));
         params.set('page', nextPage.toString());
         return params;
       },
@@ -68,7 +96,7 @@ export default function Learnings() {
     <>
       <LearningListControls titleFilter={titleFilter} onTitleFilterChange={setTitleFilter} />
       <ul className="mt-5">
-        {data.learnings.map((learning) => (
+        {props.learnings.map((learning) => (
           <li key={learning.id}>
             <Link to={`/learnings/${learning.id}`}>
               <article className="py-2 px-4 my-2 border-2 border-current rounded-md hover:bg-emerald-400">
