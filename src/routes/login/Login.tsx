9ac23ff64aa3b1a4f3fdbe239a524dcc6658614a
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2024-01-18 19:18:38 +0200

    added loading indicator to login page

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -2,6 +2,7 @@ import { Form, useActionData, useNavigation } from 'react-router-dom';
 import Input from '../../components/forms/Input';
 import Button from '../../components/Button';
 import { LoginActionResponse } from './Login.action';
+import { CenteredLoadingIndicator } from '../../components/LoadingIndicator';
 
 export default function Login() {
   const navigation = useNavigation();
@@ -13,6 +14,7 @@ export default function Login() {
         <h1 className="text-5xl font-bold text-center mb-5">wilt</h1>
         <Form method="post" className="flex flex-col gap-5 m-auto md:w-1/2 lg:w-1/3 ">
           {actionData?.status === 'error' && <p className="text-center text-red-500">{actionData.message}</p>}
+          {navigation.state === 'submitting' && <CenteredLoadingIndicator />}
           <Input id="email" name="email" label="Email" />
           <Input id="password" name="password" type="password" label="Password" />
           <Button variant="primary" disabled={navigation.state === 'submitting'}>
